1b0404f5690e5c809167ffb893e1e6b73d384067
Alessandro-Alino <alessandro.anzini@gmail.com>
2024-05-27 11:52:47 +0200

    Second commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:terant/app_multi_provider.dart';
 import 'package:terant/config/router/routes.dart';
